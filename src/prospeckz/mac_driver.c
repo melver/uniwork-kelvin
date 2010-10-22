0e9ba40e8517a9fcc812f92466b77a6f8740f3ce
Marco Elver <marco.elver@gmail.com>
2010-10-22 15:59:48 +0100

    More complete mac_driver

--- src/prospeckz/mac_driver.c
+++ src/prospeckz/mac_driver.c
@@ -1,32 +1,44 @@
 
-#include "PRS8.h"
+/* PSoc port defintions */
+#include "psocapi.h"
+#include "psocgpioint.h"
+
+/* Prospeckz api */
+#include "prospeckz.h"
 #include "prospeckz_radio.h"
+
+#include "config.h"
 #include "serial.h"
 
 #include "mac_driver.h"
 
-#define HW_ADDR	3
 
 void (*xlowpan_receive_pkt)(void *data, size_t length);
 
+/* local variables */
+
+static unsigned char rx_data[MAC_MAX_PAYLOAD];
+
+/* local functions */
+
 static void send_pkt(void *data, size_t length)
 {
-	//serial_cprint("send_pkt: sending packet");
 	Radio_transmit((unsigned char*)data, (unsigned char)length);
-	//serial_cprint("send_pkt: sending done");
+	LOGMSG("send_pkt: sending packet done");
 }
 
 static void set_receive_pkt(void (*receive_pkt)(void *data, size_t length))
 {
-	serial_cprint("called set_receive_pkt");
+	LOGMSG("called set_receive_pkt");
 	xlowpan_receive_pkt = receive_pkt;
 }
 
 /* return 64bit address; should be HW-id based. */
 static void get_addr64(struct xlowpan_addr64 *addr)
 {
-	unsigned char hw_addr = HW_ADDR;//Radio_getHWAddr();
-	serial_cprint("called get_addr64");
+	unsigned char hw_addr = HW_ADDR;
+	LOGMSG("called get_addr64");
+	
 	XLOWPAN_SET_ADDR64((*addr), 'S', 'L', 'I', 'P', 'C', 'C', hw_addr, hw_addr);
 }
 
@@ -35,7 +47,7 @@ static unsigned char make_session(void)
 {
 	static unsigned char sess = 0;
 	unsigned char newsess;
-	serial_cprint("called make_session");
+	LOGMSG("called make_session");
 	
 	do {
 		newsess = PRS8_bReadPRS();
@@ -45,10 +57,12 @@ static unsigned char make_session(void)
 	return sess;
 }
 
+/* API */
+
 struct mac_driver *mac_getdrv(void)
 {
 	static struct mac_driver drv;
-	serial_cprint("called mac_getdrv");
+	LOGMSG("called mac_getdrv");
 	
 	drv.send_pkt = send_pkt;
 	drv.set_receive_pkt = set_receive_pkt;
@@ -56,4 +70,34 @@ struct mac_driver *mac_getdrv(void)
 	drv.make_session = make_session;
 	
 	return &drv;
+}
+
+/* core radio routines */
+
+void mac_initradio(void)
+{
+	Radio_init(RADIO_CHANNEL);
+	Radio_setTxLevel(RADIO_TXLEVEL);
+	Radio_receiverOn;
+}
+
+unsigned char mac_radiohandler(void)
+{
+	/* If FIFOP pin is high there is a complete packet or the buffer has overflowed */
+	if(FIFOP_Data_ADDR & FIFOP_MASK) {
+		receiveStatus_t rx_stat;
+		
+		LOGMSG("mac_radiohandler: radio interrupt");
+			
+		/* Now get the packet */
+		if((rx_stat = Radio_receive(rx_data, MAC_MAX_PAYLOAD )).radioStatus == SUCCESS) {
+			LOGMSG("mac_radiohandler: received packet");
+			/* deliver to upper layer (xlowpan) */
+			xlowpan_receive_pkt(rx_data, rx_stat.length);
+		}
+		
+		return 1;
+	}
+	
+	return 0;
 }
--- src/prospeckz/mac_driver.h
+++ src/prospeckz/mac_driver.h
@@ -1,5 +1,15 @@
 
+#ifndef MACDRIVER_H
+#define MACDRIVER_H
+
 #include "xlowpan.h"
 
+/* callback function */
 extern void (*xlowpan_receive_pkt)(void *data, size_t length);
+
+/* API */
 struct mac_driver *mac_getdrv(void);
+void mac_initradio(void);
+unsigned char mac_radiohandler(void);
+
+#endif
